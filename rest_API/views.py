bebcb2aa5091cd02c3031c5dff58043ae8d09aaa
adkrueger <adkrueger@wpi.edu>
2020-12-18 17:39:32 -0500

    added a view for HeroSerializer

--- rest_API/views.py
+++ rest_API/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import HeroSerializer
+from .models import Hero
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all().order_by('name')
+    serializer_class = HeroSerializer
