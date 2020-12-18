dd9d884d1c6b7d4ebacd0ed94865d7a8cb0f0352
adkrueger <adkrueger@wpi.edu>
2020-12-18 18:02:44 -0500

    added a URL and router for the API

--- rest_API/urls.py
+++ rest_API/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'heroes', views.HeroViewSet)
+
+# let's wire up the API with automatic URL routing
+# we'll also include login URLs for the browsable API
+
 app_name = 'rest_API'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
